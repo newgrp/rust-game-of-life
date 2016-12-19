0ceb3be532cd99fd01b659619a45c21aa4c8ab81
birdperson-lives <rayradjr@gmail.com>
2016-12-19 15:44:27 -0600

    Copied stogit repo

---
+++ game_of_life/src/life_parallel.rs
@@ -0,0 +1,341 @@
+const NUM_THREADS: usize = 64;
+
+extern crate rand;
+extern crate piston_window;
+
+use std::cmp::{min,max};
+use std::sync::Arc;
+use std::collections::HashSet;
+use std::collections::HashMap;
+use std::thread;
+use std;
+
+use piston_window::*;
+
+#[derive(Clone)]
+struct Bounds {   
+    x_min: isize,
+    x_max: isize,
+    y_min: isize,
+    y_max: isize,
+}
+
+impl Bounds {
+    fn new() -> Bounds {
+        Bounds { x_min: 0, x_max: 0, y_min: 0, y_max: 0 }
+    }
+
+    fn update_bounds(&mut self, x:isize, y:isize) {
+        if x < self.x_min {
+            self.x_min = x;
+        }
+        if x > self.x_max {
+            self.x_max = x;
+        }
+        if y < self.y_min {
+            self.y_min = y;
+        }
+        if y > self.y_max {
+            self.y_max = y;
+        }
+    }
+
+    fn merge(&mut self, other:Bounds) {
+        self.x_min = min(self.x_min, other.x_min);
+        self.x_max = max(self.x_max, other.x_max);
+        self.y_min = min(self.y_min, other.y_min);
+        self.y_max = max(self.y_max, other.y_max);
+    }
+}
+
+fn cells_access_record(s:&str) {
+    println!("Arc::get_mut(&mut self.cells) returned None at {}", s);
+}
+
+// fn parts_access_record(k:usize, s:&str) {
+//     println!("Arc::get_mut(&mut self.parts[{}]) returned None at {}", k, s);
+// }
+
+#[derive(Clone)]
+pub struct Life {
+    pub generation: i64,
+    pub cells: Arc<HashMap<(isize, isize), i8>>,
+    parts: Vec<Arc<HashSet<(isize, isize)>>>,
+    rect: Bounds,
+    window_width:u32,
+    window_height:u32,
+}
+
+impl Life {
+    pub fn new(width:u32,height:u32) -> Life {
+        Life { generation: 0, cells: Arc::new(HashMap::new()), parts: vec![Arc::new(HashSet::new()); NUM_THREADS], rect: Bounds::new(), window_width:width,window_height:height }
+    }
+
+    pub fn set_primitive(&mut self, t:(isize,isize), v: i8) {
+        let x = t.0;
+        let y = t.1;
+        if !self.cells.contains_key(&(x,y)) {
+            if let Some(re) = Arc::get_mut(&mut self.cells) {
+                (*re).insert((x,y), v);
+            } else {
+                cells_access_record("Life::set, does not contain key");
+            }
+            let ind = rand::random::<usize>()%NUM_THREADS;
+            (*Arc::make_mut(&mut self.parts[ind])).insert((x,y));
+            // if let Some(pe) = Arc::get_mut(&mut self.parts[ind]) {
+            //     (*pe).insert((x,y));
+            // } else {
+            //     parts_access_record(ind, "Life::set, does not contain_key");
+            // }
+        }
+        else {
+            if let Some(re) = Arc::get_mut(&mut self.cells) {
+                if let Some(z) = (*re).get_mut(&(x,y)) {
+                    *z = v;
+                }
+            } else {
+                cells_access_record("Life::set, contains key");
+            }
+        }
+    }
+
+    pub fn set(&mut self, t:(isize,isize), v: i8) {
+        self.set_primitive(t,v);
+        self.cleanup();
+    }
+
+    fn get_adjacent(x:isize, y:isize) -> Vec<(isize, isize)> {
+        vec![(x+1, y  ),
+             (x+1, y+1),
+             (x  , y+1),
+             (x-1, y+1),
+             (x-1, y  ),
+             (x-1, y-1),
+             (x  , y-1),
+             (x+1, y-1)]
+    }
+
+    // fn next_val(&self, x:isize, y:isize) -> i8 {
+    //     let mut neighbors: i8 = 0;
+    //     for (i,j) in Life::get_adjacent(x,y) {
+    //         if self.cells.contains_key(&(i,j)) {
+    //             neighbors += self.cells[&(i,j)];
+    //         }
+    //     }
+    //     if (neighbors == 3) | ((neighbors == 2) & (self.cells[&(x,y)] == 1)) {
+    //         1
+    //     } else {
+    //         0
+    //     }
+    // }
+
+    fn next_val_from_arc(cells_ref:&Arc<HashMap<(isize, isize), i8>>, x:isize, y:isize) -> i8 {
+        let mut neighbors: i8 = 0;
+        for (i,j) in Life::get_adjacent(x,y) {
+            if cells_ref.contains_key(&(i,j)) {
+                neighbors += cells_ref[&(i,j)];
+            }
+        }
+        if (neighbors == 3) | ((neighbors == 2) & (cells_ref[&(x,y)] == 1)) {
+            1
+        } else {
+            0
+        }
+    }
+
+    pub fn cleanup(&mut self) {
+        self.rect.x_min = std::isize::MAX;
+        self.rect.x_max = std::isize::MIN;
+        self.rect.y_min = std::isize::MAX;
+        self.rect.y_max = std::isize::MIN;
+        let mut thread_handles = vec![];
+        for k in 0..NUM_THREADS {
+            let my_cells = self.cells.clone();
+            let my_part = self.parts[k].clone();
+            let mut temp = self.rect.clone();
+            thread_handles.push(thread::spawn(move || {
+                let mut to_add: Vec<(isize, isize)> = vec![];
+                let mut to_del: Vec<(isize, isize)> = vec![];
+                for &(x,y) in my_part.iter() {
+                    if my_cells[&(x,y)] == 1 {
+                        temp.update_bounds(x,y);
+                        for (i,j) in Life::get_adjacent(x,y) {
+                            if !my_cells.contains_key(&(i,j)) {
+                                to_add.push((i,j));
+                            }
+                        }
+                    } else {
+                        let mut barren = true;
+                        for (i,j) in Life::get_adjacent(x,y) {
+                            if my_cells.contains_key(&(i,j)) {
+                                if my_cells[&(i,j)] == 1 {
+                                    barren = false;
+                                    break;
+                                }
+                            }
+                        }
+                        if barren {
+                            to_del.push((x,y));
+                        } else {
+                            temp.update_bounds(x,y);
+                        }
+                    }
+                }
+                (temp, to_add, to_del)
+            }));
+        }
+        let mut to_adds: Vec<Vec<(isize, isize)>> = vec![];
+        let mut to_dels: Vec<Vec<(isize, isize)>> = vec![];
+        for hand in thread_handles {
+            let ret = hand.join().unwrap();
+            self.rect.merge(ret.0);
+            to_adds.push(ret.1);
+            to_dels.push(ret.2);
+        }
+        for k in 0..NUM_THREADS {
+            for &(x,y) in &to_adds[k] {
+                if !self.cells.contains_key(&(x,y)) {
+                    if let Some(re) = Arc::get_mut(&mut self.cells) {
+                        (*re).insert((x,y), 0);
+                    } else {
+                        cells_access_record("Life::cleanup, inserting new cells");
+                    }
+                    self.rect.update_bounds(x,y);
+                    let ind = rand::random::<usize>()%NUM_THREADS;
+                    (*Arc::make_mut(&mut self.parts[ind])).insert((x,y));
+                    // if let Some(pe) = Arc::get_mut(&mut self.parts[ind]) {
+                    //     (*pe).insert((x,y));
+                    // } else {
+                    //     parts_access_record(ind, "Life::cleanup, inserting new cells");
+                    // }
+                }
+            }
+            for &(x,y) in &to_dels[k] {
+                if let Some(re) = Arc::get_mut(&mut self.cells) {
+                    (*re).remove(&(x,y));
+                } else {
+                    cells_access_record("Life::cleanup, removing cells");
+                }
+                (*Arc::make_mut(&mut self.parts[k])).remove(&(x,y));
+                // if let Some(pe) = Arc::get_mut(&mut self.parts[k]) {
+                //     (*pe).remove(&(x,y));
+                // } else {
+                //     parts_access_record(k, "Life::cleanup, removing cells");
+                // }
+            }
+        }
+    }
+
+    pub fn advance(&mut self) {
+        let mut thread_handles = vec![];
+        for k in 0..NUM_THREADS {
+            let my_cells = self.cells.clone();
+            let my_part = self.parts[k].clone();
+            thread_handles.push(thread::spawn(move || {
+                let mut cells_new = HashMap::new();
+                for &(x,y) in my_part.iter() {
+                    cells_new.insert((x,y), Life::next_val_from_arc(&my_cells,x,y));
+                }
+                cells_new
+            }));
+        }
+        let mut cells_new: Vec<HashMap<(isize, isize), i8>> = vec![];
+        for hand in thread_handles {
+            cells_new.push(hand.join().unwrap());
+        }
+        for k in 0..NUM_THREADS {
+            for (&(x,y),v) in &cells_new[k] {
+                if let Some(re) = Arc::get_mut(&mut self.cells) {
+                    if let Some(z) = (*re).get_mut(&(x,y)) {
+                        *z = *v;
+                    }
+                } else {
+                    cells_access_record("Life::advance");
+                }
+            }
+        }
+        self.cleanup();
+        self.generation += 1;
+    }
+
+    pub fn draw(&self, window:&mut PistonWindow, e:&Event,zoom:f64,offsetX:f64,offsetY:f64) {
+ 
+        window.draw_2d(e, |c, g| {
+            clear([1.0, 1.0, 1.0, 1.0], g);
+ 
+            let half_width:f64 = (self.window_width as f64)/2.0;
+            let half_height:f64 = (self.window_height as f64)/2.0;
+            let transform = c.transform.trans(offsetX,offsetY)
+                                       .trans(half_width,half_height)
+                                       .zoom(zoom)
+                                       .trans(-half_width,-half_height);
+ 
+            let mut y = self.rect.y_max;
+            let mut x: isize;
+ 
+            while y >= self.rect.y_min {
+ 
+                x = self.rect.x_min;
+                while x <= self.rect.x_max {
+                    if self.cells.contains_key(&(x,y)) {
+                        if self.cells[&(x,y)] == 1 {
+                            // Alive 
+                            rectangle([1.0, 0.0, 0.0, 1.0], // red
+                                       [x as f64 + half_width, y as f64 + half_height, 1.0 ,1.0], // rectangle
+                                       transform, g);
+                            
+                        } else {
+                            // Dead
+ 
+                        } 
+                    } else {
+                        // Dead
+ 
+                    }
+                    x += 1;
+                }
+                
+                y -= 1;
+            }
+            c.reset();
+        });
+ 
+    }
+
+    pub fn display(&self) {
+        let mut y = self.rect.y_max;
+        let mut x: isize;
+        let mut line: String;
+        println!("generation {}", self.generation);
+        while y >= self.rect.y_min {
+            line = "".to_string();
+            x = self.rect.x_min;
+            while x <= self.rect.x_max {
+                if self.cells.contains_key(&(x,y)) {
+                    if self.cells[&(x,y)] == 1 {
+                        line.push_str("*");
+                    } else {
+                        // let mut n:usize = 233;
+                        // for k in 0..NUM_THREADS {
+                        //     if self.parts[k].contains(&(x,y)) {
+                        //         n = k;
+                        //     }
+                        // }
+                        // if n < 10 {
+                        //     line += &format!("0{}", n);
+                        // } else {
+                        //     line += &format!("{}", n);
+                        // }
+                        line.push_str(" ");
+                    }
+                } else {
+                    line.push_str(" ");
+                }
+                x += 1;
+            }
+            println!("{}", line);
+            y -= 1;
+        }
+        println!("\n");
+    }
+}
---
+++ game_of_life/src/main.rs
@@ -0,0 +1,260 @@
+#![allow(non_snake_case)]
+
+extern crate piston_window;
+extern crate time;
+extern crate gfx_graphics;
+
+use std::env;
+use std::io::prelude::*;
+use std::fs::File;
+
+use piston_window::*;
+
+const WINDOW_WIDTH:u32 = 600;
+const WINDOW_HEIGHT:u32 = 400;
+
+mod life_sequential;
+mod life_parallel;
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    let mut mode = "single".to_string();
+    let mut zoom = 1.0;
+    let mut offsetX = 0.0;
+    let mut offsetY = 0.0;
+    let mut name = "Conrad's Game of Life";
+    let mut file_to_load = "src/pento.cells".to_string();
+
+    if args.len() > 1 {
+        mode = args[1].parse().unwrap();
+    }
+    if args.len() > 2 {
+        zoom = args[2].parse().unwrap();
+    }
+    if args.len() > 3 {
+        file_to_load = args[3].parse().unwrap();
+    }
+
+    let mut r_pentomino = life_sequential::Life::new(WINDOW_WIDTH,WINDOW_HEIGHT);
+    let mut r_pentomino_parallel = life_parallel::Life::new(WINDOW_WIDTH,WINDOW_HEIGHT);
+
+    // Read pattern from file 
+    let mut cells_file = File::open(file_to_load).unwrap();
+    let mut primer_data = String::new();
+    cells_file.read_to_string(&mut primer_data);
+    let mut lines = primer_data.split("\n");
+    let mut row = 0;
+    let mut column = 0;
+    for l in lines {
+        if l != "" {
+            let first_char = l.chars().nth(0).unwrap();
+            // Skip comments 
+            if first_char != '!' {
+                //This is a data line 
+                column = 0; //Reset column 
+
+                //Iterate over chars 
+                for c in l.chars() {
+                    if c == 'O' {
+                        println!("Set {},{}", column,row);
+                        if mode == "single" {
+                            r_pentomino.set_primitive((column,row), 1);
+                        } else {
+                            r_pentomino_parallel.set_primitive((column,row), 1);
+                        }
+                        
+                    }
+
+                    column += 1;
+                }
+
+                //Increment row by 1 
+                row += 1;
+            }
+        }
+        
+    }
+
+    r_pentomino.cleanup();
+    r_pentomino_parallel.cleanup();
+    
+
+    if mode == "single" {
+        r_pentomino.generation = 0;
+        // r_pentomino.set((0,0), 1);
+        // r_pentomino.set((0,1), 1);
+        // r_pentomino.set((1,1), 1);
+        // r_pentomino.set((-1,0), 1);
+        // r_pentomino.set((0,-1), 1);
+    } else {
+        r_pentomino_parallel.generation = 0;
+        // r_pentomino_parallel.set((0,0), 1);
+        // r_pentomino_parallel.set((0,1), 1);
+        // r_pentomino_parallel.set((1,1), 1);
+        // r_pentomino_parallel.set((-1,0), 1);
+        // r_pentomino_parallel.set((0,-1), 1);
+        name = "Conrad's Game of Life - Parallel"
+    }
+    
+
+    let mut windowSettings = WindowSettings::new(name, [WINDOW_WIDTH,WINDOW_HEIGHT]);
+
+    let mut window: PistonWindow = windowSettings.build().unwrap();
+
+
+    let mut events = window.events();
+
+    let mut running_average_time = 0.0;
+    let mut running_average_count = 0.0;
+
+    let font_path = "src/Quicksand-Regular.ttf";
+    let factory = window.factory.clone();
+    let mut glyphs = Glyphs::new(font_path, factory).unwrap();
+
+    let mut running = true;
+    let mut start_time = 0.0;
+    let mut time_taken = 0.0;
+
+    let mut mouse_pos:[f64;2] = [0.0,0.0];
+    let mut mouse_middle_down = false;
+    let mut mouse_last_pos:[f64;2] = [0.0,0.0];
+    let mut prevOffsetX = offsetX;
+    let mut prevOffsetY = offsetY;
+
+    while let Some(e) = events.next(&mut window) {
+
+        if let Some(Button::Keyboard(key)) = e.press_args() {
+           if key == Key::Space {
+             running = !running;
+           }
+        };
+
+        if let Some(Button::Mouse(mouse_btn)) = e.release_args() {
+            if mouse_btn == MouseButton::Middle {
+                //Stop moving 
+                mouse_middle_down = false;
+            }
+        };
+
+        if let Some(Button::Mouse(mouse_btn)) = e.press_args() {
+           let x = (((mouse_pos[0] - offsetX) - (WINDOW_WIDTH as f64/2.0)) / zoom).floor() as isize;
+           let y = (((mouse_pos[1] - offsetY) - (WINDOW_HEIGHT as f64/2.0)) / zoom).floor() as isize;
+
+           if mouse_btn == MouseButton::Middle {
+              mouse_middle_down=true;
+           }
+           if mouse_btn == MouseButton::Left {
+              // Set Alive
+              if mode == "single" {
+                r_pentomino.set((x,y),1); 
+              } else {
+                r_pentomino_parallel.set((x,y),1); 
+              }
+            
+           }
+           if mouse_btn == MouseButton::Right {
+              // Set Dead
+              if mode == "single" {
+                r_pentomino.set((x,y),0); 
+              } else {
+                r_pentomino_parallel.set((x,y),0); 
+              }
+           }
+        };
+       
+        if let Some(mot) = e.mouse_cursor_args(){
+            mouse_pos =  mot;
+            if mouse_middle_down == false {
+                mouse_last_pos = mot;
+                prevOffsetX = offsetX;
+                prevOffsetY = offsetY;
+            }
+            if mouse_middle_down == true {
+                offsetX = mot[0] - mouse_last_pos[0] + prevOffsetX;
+                offsetY = mot[1] - mouse_last_pos[1] + prevOffsetY;
+            }
+        };
+
+        if let Some(scroll) = e.mouse_scroll_args(){
+            let zoom_power = 0.1;
+            if scroll[1] == 1.0 {
+                zoom += zoom_power;
+            }
+            if scroll[1] == -1.0 {
+                zoom -= zoom_power;
+            }
+        };
+
+        if let Some(r) = e.render_args() {
+            //Render!
+
+            if running {
+                start_time = time::precise_time_ns() as f64;
+                if mode == "single" {
+                    r_pentomino.advance(); 
+                } else {
+                    r_pentomino_parallel.advance(); 
+                }
+                time_taken =  ((time::precise_time_ns() as f64 - start_time) as f64) / 1000000000.0;
+                running_average_count += 1.0;
+                running_average_time += time_taken;
+            }
+            
+
+            //Draw the grid 
+            if mode == "single" {
+                r_pentomino.draw(&mut window,&e,zoom,offsetX,offsetY);
+            } else {
+                r_pentomino_parallel.draw(&mut window,&e,zoom,offsetX,offsetY);
+            }
+            
+            let mut generation = r_pentomino.generation;
+            if mode == "parallel" {
+                generation = r_pentomino_parallel.generation;
+            }
+
+            let displayOne:String = format!("Average time per generation: {0:.4} seconds.", running_average_time / running_average_count);  
+            let displayTwo:String = format!("Time taken for last generation: {0:.4} seconds.", time_taken);  
+            let displayThree:String = format!("Generation: {}", generation);  
+
+             //Render text
+             window.draw_2d(&e, |c, g| {
+                let X = 5.0;
+                let Y = 15.0;
+                let line_spacing = 15.0;
+
+                let mut transform = c.transform.trans(X, Y);
+                text::Text::new_color([0.0, 0.0, 0.0, 1.0], 11).draw(
+                    &displayOne,
+                    &mut glyphs,
+                    &c.draw_state,
+                    transform, g
+                );
+
+                transform = c.transform.trans(X, Y+line_spacing);
+                text::Text::new_color([0.0, 0.0, 0.0, 1.0], 11).draw(
+                    &displayTwo,
+                    &mut glyphs,
+                    &c.draw_state,
+                    transform, g
+                );
+
+                transform = c.transform.trans(X, Y+line_spacing*2.0);
+                text::Text::new_color([0.0, 0.0, 0.0, 1.0], 11).draw(
+                    &displayThree,
+                    &mut glyphs,
+                    &c.draw_state,
+                    transform, g
+                );
+
+
+            });
+        }
+
+        if let Some(u) = e.update_args() {
+            //Update
+        }
+    }
+
+}
